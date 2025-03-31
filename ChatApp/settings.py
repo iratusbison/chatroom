e335c771258a625de1a1cc8c5e59f5c9238eb116
iratusbison <iratusbison@gmail.com>
2025-03-31 08:57:56 +0530

    COLLECTSTATIC CONFIG

--- ChatApp/settings.py
+++ ChatApp/settings.py
@@ -24,11 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bzm&ppgi-7dsqp%gqf$*&)26-6qa2h5&9&$c5f20kafwc9m#jx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['https://vpvschat.onrender.com','vpvschat.onrender.com','127.0.0.1:8000','127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['https://vpvschat.onrender.com']
+#CSRF_TRUSTED_ORIGINS = ['https://vpvschat.onrender.com']
 
 CHANNEL_LAYERS = {
 	"default": {
@@ -134,13 +134,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, '/staticfiles/')
 STATIC_URL = '/static/'
-
-'''STATICFILES_DIRS = [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'staticfiles')
 
-]'''
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
